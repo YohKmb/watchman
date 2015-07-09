287221ce536b751affa4997f0bbf53272ea00978
YohKmb <yoh134shonan@gmail.com>
2015-07-09 15:50:13 +0900

    moved graphs into content div section

--- watchman.py
+++ watchman.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, Response #, request
+
+from flask import Flask, render_template, Response
 import json
 from lib import pinger
-from threading import current_thread
 
 
 app = Flask(__name__)
@@ -18,30 +18,16 @@ def history():
         [{"host":k, "history":list(v)} for k,v in hists.items() ]
     ))
 
-
 @app.route("/main")
 def main_page():
     return render_template("main.html")
 
 
-
-@app.route("/test")
-def test():
-    # print "app : " + str(current_thread() )
-    return render_template("test2.html")
-
-
 if __name__ == "__main__":
-    print "main : " + str(current_thread() )
-
     senders, receiver = pinger.generate_pingers(targets=["www.kernel.org", "web.mit.edu"])
-    print "senders = " + str(senders)
-    print "receiver = " + str(receiver)
 
     try:
         pinger.start_pingers(senders, receiver, is_fg=False)
         app.run(debug=False)
-        # app.run(debug=True)
     finally:
         pinger.stop_pingers(senders, receiver)
-
